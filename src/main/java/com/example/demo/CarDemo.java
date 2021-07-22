5ef625abd8a5158870e453ee5e04a8f25f81d754
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-22 19:17:25 +0530

    commit

--- src/main/java/com/example/demo/CarDemo.java
+++ src/main/java/com/example/demo/CarDemo.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 public class CarDemo {
 
+	void carRepair(){
+		System.out.println("Car Repair ");
+	}
+	
+	
 	public static void main(String[] args) {
 		
+		CarDemo d=new CarDemo();
+		d.carRepair();
 	}
 	
 	
