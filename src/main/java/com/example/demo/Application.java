ee16bc250ab349dfc5468551d3f89967973358cf
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-23 15:38:42 +0530

    push to feature-200

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -11,8 +11,13 @@ public class Application {
 	}
 
 void m1(){
-SpringApplication.run(Application.class, args);
+SpringApplication.run("m2() method ");
 }
+
+void m2(){
+SpringApplication.run("m1() method ");
+}
+
 
 
 }
