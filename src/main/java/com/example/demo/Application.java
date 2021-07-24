7320f955c945a30e5501f8a09ee0dedc7406163d
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-24 22:04:05 +0530

    message

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,18 +9,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 void m1(){
-
 System.out.println("m1() method ");
-SpringApplication.run("m2() method ");
-
 }
-
 void m2(){
-SpringApplication.run("m1() method ");
 }
-
-
-
 }
