40d3d1a532e00e84c5b10b77a532c0995ff7648e
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-25 11:12:06 +0530

    feature-200 added data

---
+++ src/main/java/com/app/demo/PurchaseData.java
@@ -0,0 +1,11 @@
+package com.app.demo;
+//feature data added
+public class PurchaseData {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("purchase are included in here");
+	}
+}
