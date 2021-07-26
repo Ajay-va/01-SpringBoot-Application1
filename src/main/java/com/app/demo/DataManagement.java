8f66ef0d7a7b9b5d12ffd9601ddce3a7b067dae7
Ajay-va <javvajiajayvarma222@gmail.com>
2021-07-26 11:42:41 +0530

    fixed data to feature-200

--- src/main/java/com/app/demo/DataManagement.java
+++ src/main/java/com/app/demo/DataManagement.java
@@ -5,6 +5,35 @@ public class DataManagement {
 	private int did;
 	private String productDetails;
 	private double productMails;
+	public DataManagement(int did, String productDetails, double productMails) {
+		super();
+		this.did = did;
+		this.productDetails = productDetails;
+		this.productMails = productMails;
+	}
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
+	}
+	public String getProductDetails() {
+		return productDetails;
+	}
+	public void setProductDetails(String productDetails) {
+		this.productDetails = productDetails;
+	}
+	public double getProductMails() {
+		return productMails;
+	}
+	public void setProductMails(double productMails) {
+		this.productMails = productMails;
+	}
+	@Override
+	public String toString() {
+		return "DataManagement [did=" + did + ", productDetails=" + productDetails + ", productMails=" + productMails
+				+ "]";
+	}
 	
 
 }
